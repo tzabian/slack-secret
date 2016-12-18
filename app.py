cc6a9a037d223714fbb77aa36cb9b9ef64717ffb
Tariq Zabian <tzabian@gmail.com>
2016-12-18 23:43:48 +0400

    added onetimesecret calls

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
 import os
+import requests
 
+from requests.auth import HTTPBasicAuth
 from flask import Flask, request, Response
 
 slack_token = os.environ.get('SLACK_TOKEN')
+ttl = os.environ.get('SECRET_TTL', 259200)  # 3 days
+secret_user = os.environ.get('SECRET_USER')
+secret_pass = os.environ.get('SECRET_PASS')
+
+SECRET_BASE = "https://onetimesecret.com"
+SECRET_LINK = "{}/secret/".format(SECRET_BASE)
+SECRET_SHARE = "{}/api/v1/share".format(SECRET_BASE)
 
 app = Flask(__name__)
 
@@ -11,12 +20,16 @@ app = Flask(__name__)
 def secret():
 
     token = request.values.get('token')
+    text = request.values.get('text')
 
     if token != slack_token:
         return Response('You are not authorized to use slack-secret',
                         content_type='text/plain; charset=utf-8')
 
-    return request.values.get('text')
+    d = {'secret': text, 'ttl': ttl}
+    r = requests.post(SECRET_SHARE, data=d, auth=HTTPBasicAuth(secret_user, secret_pass))
+
+    return Response(r.text)
 
 
 if __name__ == '__main__':
